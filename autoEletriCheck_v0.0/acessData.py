e674b3a930fce1b60d33ea5b695dd6a26edfd2d3
inertia1p <87255742@qq.com>
2018-10-26 10:15:03 +0800

    10.14
    第一次测试版

---
+++ autoEletriCheck_v0.0/acessData.py
@@ -0,0 +1,11 @@
+﻿import requests
+from lxml import html
+url='https://wxschool.lsmart.cn/electric/electric_index.shtml?openId=oujX2w6dzwgLOXs012-qhiM0IlPQ&wxArea=10354'
+
+def getData():
+    response = requests.get(url).content
+    selector = html.fromstring(response)
+    data = selector.xpath("//div[@class='w_n_lis2']/p/text()")[0]
+    data = data.encode('utf-8').decode().replace(" 度","")
+    return float(data)
+
---
+++ autoEletriCheck_v0.0/autoEngeyAlert.py
@@ -0,0 +1,10 @@
+﻿from acessData import getData
+from sendMail import mail
+import time 
+
+if __name__ =='__main__':
+    while True:
+        data = getData()
+        if data<10.0:
+            mail()
+        time.sleep(100)
